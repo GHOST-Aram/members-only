2673c4df25743af1d34f27faf3821ea70156dad5
GHOST-Aram <owinoojala@gmail.com>
2023-08-03 11:08:33 +0300

    feat: Validate membership code setting form with javascript

---
+++ static/scripts/membership.js
@@ -0,0 +1,38 @@
+const form = document.querySelector('form')
+const textbox = document.querySelector('input')
+const error = document.querySelector('.error')
+
+
+textbox.addEventListener('input', (event) =>{
+    // Check 
+    if(textbox.validity.valid){
+        error.textContent = ''
+    } else {
+        
+        if(textbox.validity.patternMismatch){
+            error.textContent = 'The code can only have letter characters.'
+        }
+        
+        if(textbox.validity.tooLong){
+            error.textContent = `Please provide a code of length 
+            less than or equal to ${textbox.maxLength}`
+        }
+        
+        if(textbox.validity.tooShort){
+            error.textContent = `Please provide a code of length 
+            greater than or equal to ${textbox.minLength}`
+        }
+        
+        if(textbox.validity.valueMissing){
+            error.textContent = 'This is a required field. You cannot leave it blank.' 
+        } 
+    }
+})
+
+form.addEventListener('submit', (event) =>{
+    if(!textbox.validity.valid){
+        event.preventDefault()
+    } else {
+        form.submit()
+    }
+})
